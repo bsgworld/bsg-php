4361c158ded83af48dd47fe171f042459b7f3ab1
Dmitriy Blyzniuk <fantom79022@gmail.com>
2017-03-03 16:41:39 +0200

    refactor

--- tests/TestConfig.php
+++ tests/TestConfig.php
@@ -4,6 +4,6 @@ class TestConfig {
     const TEST_PHONE_1 = '380501111111';
     const TEST_PHONE_2 = '380501111112';
     const SMS_SENDER_NAME = 'name';
-    const VIBER_SENDER_NAME = 'viber sender';
+    const VIBER_SENDER_NAME = 'viber_sender';
     const EXCEPTION_FAIL = 'Request failed. Error: ';
 }
