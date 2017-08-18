1cbbc15223f7577e7ca8b8184c77598ed1fbfd31
fantom79022 <fantom79022@gmail.com>
2017-08-18 16:59:02 +0300

    Update ApiClient.php

--- src/BSG/ApiClient.php
+++ src/BSG/ApiClient.php
@@ -26,9 +26,9 @@ class ApiClient {
     public function sendRequest ($resource_url, $post_data=NULL, $custom_request=NULL) {
         $client = curl_init();
         if ($post_data === NULL || !is_array($post_data))
-            curl_setopt($client,CURLOPT_HTTPHEADER,array('X-API-KEY: '.$this->api_key,'Content-type: text/json; charset=utf-8'));
+            curl_setopt($client,CURLOPT_HTTPHEADER,array('X-API-KEY: '.$this->api_key, 'X-API-SOURCE: ' . $this->api_source, 'Content-type: text/json; charset=utf-8'));
         else
-            curl_setopt($client,CURLOPT_HTTPHEADER,array('X-API-KEY: '.$this->api_key, 'X-API-SOURCE' . $this->api_source));
+            curl_setopt($client,CURLOPT_HTTPHEADER,array('X-API-KEY: '.$this->api_key, 'X-API-SOURCE: ' . $this->api_source));
         curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($client, CURLOPT_FOLLOWLOCATION, false);
         if ($custom_request !== NULL)
